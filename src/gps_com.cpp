14e36a7777bd3db7dc0a3f023c86b1674577a207
Sebastian Woetzel <wose@zuendmasse.de>
2013-05-06 18:10:03 +0200

    more debug infos

--- src/gps_com.cpp
+++ src/gps_com.cpp
@@ -41,6 +41,8 @@ void gps_com::update()
             {
               std::cout << "got data" << std::endl;
               std::lock_guard<std::mutex> lock(_gps_data_mutex);
+              if(new_data->set & LATLON_SET)
+                std::cout << "latlon is set" << std::endl;
               _last_data = *new_data;
             }
         }
